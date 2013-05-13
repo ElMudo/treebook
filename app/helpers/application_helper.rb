624c7726a5db5676ea377c937fb315e799cd6376
Conor Wyse <el.mudo@xs4all.nl>
2013-05-13 15:20:46 +0200

    Styled the flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def flash_class(type)
+    case type
+    when :notice
+      return "alert-success"
+    when :alert
+      return "alert-error"
+    else
+      return ""
+    end
+  end
 end
